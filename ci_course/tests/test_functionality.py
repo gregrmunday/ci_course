e6cc4915c6661aca6d4c835f7d12b8f6cce8c5df
Gregory Munday <gregory.munday@gmail.com>
2025-10-20 11:38:28 +0100

    Add coverage

--- ci_course/tests/test_functionality.py
+++ ci_course/tests/test_functionality.py
@@ -16,6 +16,7 @@ def test_minimum():
     Test the function `minimum` in functionality.py
     """
     assert ci_course.minimum(3j, 4j) is None
+    assert ci_course.minimum(3, 4j) == 3
     assert ci_course.minimum("bob", "cat") is None
     assert ci_course.minimum(1, 2, 3) == 1
     assert ci_course.minimum(1.2, 2.3) == 1.2
